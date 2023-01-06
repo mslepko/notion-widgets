47f32e74f021ac3925d88d31af99ad6dc6ba458e
Michal Slepko <michal.slepko@gmail.com>
2023-01-06 22:20:54 +0000

    Added vite-plugin-ssr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ssr()],
 })
