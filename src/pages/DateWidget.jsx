6363392a99ed33fd6d01e2b9ca0d32c11b156d50
Michal Slepko <michal.slepko@gmail.com>
2023-01-09 16:50:58 +0000

    Updated date widget page

--- src/pages/DateWidget.jsx
+++ src/pages/DateWidget.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
+import DateTime from '../components/DateTime'
+
 function DateWidget() {
     return (
-        <div>
-            <h1>This is the DateWidget page</h1>
-        </div>
+        <DateTime />
     )
 }
 
