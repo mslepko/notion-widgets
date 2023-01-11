dda109aaa646fbabf55c4c6c4ab9f9882c51c43c
Michal Slepko <michal.slepko@gmail.com>
2023-01-11 21:06:25 +0000

    Layout updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav className="flex flex-col relative h-screen min-w-[20vw] items-center justify-center shrink-0">
+    <nav className="flex flex-col relative min-h-screen min-w-[20vw]
+    items-center justify-center shrink-0
+    text-dark dark:text-light bg-light dark:bg-brand">
       <Link to="/">
         Home
       </Link>
