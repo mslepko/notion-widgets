640ed0ce4027f886edaaed982045eb553760838b
Michal Slepko <michal.slepko@gmail.com>
2023-01-11 21:31:54 +0000

    Toggle component updates

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,29 +1,22 @@
 import { useState } from "react";
 
 export default function Toggle() {
-    const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(false);
 
-    return (
-        <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-            <div className="flex">
-                <label class="inline-flex relative items-center mr-5 cursor-pointer">
-                    <input
-                        type="checkbox"
-                        className="sr-only peer"
-                        checked={enabled}
-                        readOnly
-                    />
-                    <div
-                        onClick={() => {
-                            setEnabled(!enabled);
-                        }}
-                        className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
-                    ></div>
-                    <span className="ml-2 text-sm font-medium text-gray-900">
-                        {enabled ? 'ON' : 'OFF'}
-                    </span>
-                </label>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex relative">
+      <div
+        onClick={() => {
+          setEnabled(!enabled);
+        }}
+        className={
+          "w-11 h-6 bg-mid rounded-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all"
+          + (enabled ? 'bg-brand after:translate-x-full after:border-brand after:bg-brand' : '')}
+
+      ></div>
+      <span className="ml-2 text-sm font-medium text-dark-900">
+        {enabled ? 'ON' : 'OFF'}
+      </span>
+    </div>
+  );
 }
