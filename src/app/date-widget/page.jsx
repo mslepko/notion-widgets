e179a2a775b12656272111d17e4e2541db343788
Michal Slepko <michal@rootscope.co.uk>
2023-03-24 00:32:57 +0000

    updated the url #46

--- src/app/date-widget/page.jsx
+++ src/app/date-widget/page.jsx
@@ -9,15 +9,17 @@ function DateWidgetPage() {
   const dateWidgetConfig = DateWidgetConfig()
   const searchParams = useSearchParams()
   const pathname = usePathname();
-
   const hideNav = searchParams.has('hidenav') && searchParams.get('hidenav')  == 1
-  
+  let copyUrl = window.location.origin + pathname + '?hidenav=1'
+  if (searchParams.toString().length > 0) {
+    copyUrl += '&' + searchParams.toString()
+  }
+    
   return (
-    <div className="flex flex-col gap-10">
-      <div className="flex flex-col md:flex-row w-3/4 justify-around">
+    <div className="flex flex-col gap-10 w-3/4">
+      <div className="flex flex-col md:flex-row w-full justify-around">
         {!hideNav && <div className='w-1/2'>
           <h3 className='mb-4 text-dark'>Date/time settings</h3>
-          <button onClick={()=> setConfig('blue')}>Set color to blue</button>
           <DateTimeConfig 
             config={dateWidgetConfig}
             />
@@ -29,7 +31,7 @@ function DateWidgetPage() {
       </div>
       {!hideNav && <div>
         <p>URL to copy</p>
-        <p>{window.location.href}&hidenav=1</p>
+        <p>{copyUrl}</p>
       </div>}
     </div>
   )
