c8a619d67b4ca74a45e9d8ca130027b44d5764a8
Michal Slepko <michal@rootscope.co.uk>
2023-03-24 12:44:19 +0000

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import Nav from './components/Nav'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Notion Widgets',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
         <main className="flex w-full min-h-screen min-w-screen flex-1 items-center justify-center text-2xl text-dark">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   )
